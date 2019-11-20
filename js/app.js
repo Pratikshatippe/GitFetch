87b17d9918f698057eafd6df0c49c65cf6034b86
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-20 10:04:17 +0530

    display on console

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-    $('#searchuser').on('keyup', function(){
-        console.log("key pressed");
+    $('#searchuser').on('keyup', function(e){
+       let username = e.target.value;
+
+       $.ajax({
+           url:'https://api.github.com/users/'+username,
+           data:{
+            client_id:'a15ec5ffe35deb24771f',
+            client_secret:'8bcc4e3b62d9fe96b36d3aeab785476c1e914907'
+           } 
+       }).done(function(user){
+        //    console.log(user);
+        
+       })
     });
 });
